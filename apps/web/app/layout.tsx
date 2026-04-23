0469afc3bf388cd6ba8550b27fc4aac31f8d5194
Esteban Lasso <estebannl28@gmail.com>
2026-04-24 00:30:31 +0200

    fix

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -25,8 +25,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable}`} >
         <AuthProvider>
           <Providers>
             <WebSocketProvider>
